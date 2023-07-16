1683b83c59e874ac978c7a2974c158d89272e1d9
alamin2016mdy <alamin2016mdy@gmail.com>
2023-07-17 00:20:56 +0600

    js

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var MyName = 'Al amin';
 console.log(MyName);
-console.log(typeof MyName);
+console.log(typeof MyName); // Variable type দেখার জন্য [ typeof ] ব্যবহার করা হয়।
 
 var MyPhoneNumber = +8801319639176;
 console.log(MyPhoneNumber);
@@ -8,8 +8,8 @@ console.log(typeof MyPhoneNumber);
 
 var lorem10 = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, rem!';
 console.log(lorem10);
-console.log(lorem10.toLowerCase());
-console.log(lorem10.toUpperCase());
+console.log(lorem10.toLowerCase()); //লেটার ছোট-হাতের করার জন্য [ .toLowerCase()]
+console.log(lorem10.toUpperCase()); //লেটার বড়-হাতের করার জন্য [ .toUpperCase() ]
 console.log(lorem10.split(' '));
 console.log(lorem10.indexOf("sit"));
 
@@ -27,8 +27,8 @@ var total = number3 + number4;
 total = total.toFixed(3);
 console.log(total);
 
-var number5 = 2; 
-number5 = ''+number5;
+var number5 = 2;
+number5 = '' + number5;
 console.log(typeof number5);
 
 var number6 = 85;
@@ -53,9 +53,9 @@ var number13 = 70;
 var total = number12 % number13;
 console.log(total);
 
-var FastName = 'Al Amin';
+var FirstName = 'Al Amin';
 var LastName = 'Khan';
-var total = FastName + ' ' + LastName;
+var total = FirstName + ' ' + LastName;
 console.log(total);
 
 var number = 5.6564555555;
@@ -70,5 +70,51 @@ var number14 = Math.random() * 40;
 var result3 = Math.round(number14);
 console.log(result3);
 
+var biscuitPrice = 15
+if (biscuitPrice < 10) { console.log("biscuit খাবো"); }
+else { console.log("খালি চা খাবো"); }
+
+var জব_পাইছো = true;
+var জমানো_টাকা = 5;
+if (জব_পাইছো == true && জমানো_টাকা > 50000){console.log("চলো তোমার বিয়ের বাবস্থা করি");}
+else if (জব_পাইছো == true){console.log("কিছু দিন পর থেকে খুঁজবো");}
+else {console.log("তোমার কপালে বিয়ে নাই");};
+
+var firendsAge = [12, 15, 58, 22, 45, 32, 22, 21];
+var razAge = firendsAge[3];
+console.log(razAge);
+var position = firendsAge.indexOf(22);
+console.log(position);
+firendsAge.push(33, 41, 42, 33);
+firendsAge.push(23, 24);
+console.log(firendsAge.length);
+firendsAge.pop(58);
+firendsAge.shift();
+firendsAge.unshift(31);
+var part = firendsAge.slice(5)
+console.log(part)
+console.log(firendsAge);
+
+var num = 15;
+while (num <= 20){
+    console.log (num);
+    num++
+}
+
+var nums = [55, 21, 33, 78, 54, 12, 36, 42]
+for (var i = 0;  i< nums.length; i++){
+    var element = nums[i];
+    console.log(element)
+}
+
+
+
+
+
+
+
+
+
+ 
 
 
